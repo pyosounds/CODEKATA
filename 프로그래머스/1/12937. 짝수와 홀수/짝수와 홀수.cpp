59dc605b42822edf0390cb25c0bd7b4677df7cdc
pyosounds <156515622+pyosounds@users.noreply.github.com>
2025-08-19 09:37:32 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int num) {
+    string answer = "";
+
+    if (abs(num) % 2 == 0)
+    {
+        answer = "Even";
+    }
+    else if (abs(num) % 2 == 1)
+    {
+        answer = "Odd";
+    }
+
+    return answer;
+}
