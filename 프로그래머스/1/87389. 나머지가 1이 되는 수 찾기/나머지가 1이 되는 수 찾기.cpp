8aed222196d8ad5acbc0eee4c949c8b1e792a09c
pyosounds <156515622+pyosounds@users.noreply.github.com>
2025-08-20 09:32:34 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 2.38 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int solution(int n)
+{
+    int answer = 0;
+    
+    for (int i = 1; i <= 1000000; ++i)
+    {
+        if (n % i == 1)
+        {
+            answer = i;
+            break;
+        }
+    }
+    
+    return answer;
+}
