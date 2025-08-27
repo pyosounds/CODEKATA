8cc25394d710e4d70045a43247f5c6f94f83d8b8
pyosounds <156515622+pyosounds@users.noreply.github.com>
2025-08-27 09:07:44 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.02 ms, Memory: 4.04 MB -BaekjoonHub

---
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string s)
+{
+    string answer = s;
+    
+    sort(answer.begin(), answer.end(), greater<>());
+    
+    return answer;
+}
