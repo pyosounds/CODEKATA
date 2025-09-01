216a787c1e7ffb21d562d69e1ebc2aece27b8fef
pyosounds <156515622+pyosounds@users.noreply.github.com>
2025-09-01 12:10:34 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4156 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.cc
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -7,7 +7,7 @@ queue<int> Q;
 int main()
 {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	
+
 	cin >> N;
 
 	for (int i = 1; i <= N; ++i)
@@ -18,8 +18,7 @@ int main()
 	while (1 != Q.size())
 	{
 		// 제일 위에 있는 카드를 바닥에 버린다.
-		Q.pop();
-		if (1 == Q.size()) break;
+		Q.pop();		
 
 		// 제일 위에 있는 카드를 제일 아래에 있는 카드 밑으로 옮긴다.
 		Q.push(Q.front());
