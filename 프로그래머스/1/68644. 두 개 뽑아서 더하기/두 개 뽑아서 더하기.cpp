3ff7fc8cabbf1757f4e039da9e0913e6134bc9f5
pyosounds <156515622+pyosounds@users.noreply.github.com>
2025-09-05 09:19:28 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.27 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers)
+{
+    vector<int> answer;
+    set<int> s;
+    
+    for (int i = 0; i < numbers.size() - 1; ++i)
+    {
+        for (int j = i + 1; j < numbers.size(); ++j)
+        {
+            s.insert(numbers[i] + numbers[j]);
+        }
+    }
+    
+    for (const auto& e : s)
+    {
+        answer.push_back(e);
+    }    
+    
+    return answer;
+}
