f9b0f1c3cb88187310b7e474067241dd233fce76
pyosounds <156515622+pyosounds@users.noreply.github.com>
2025-09-16 09:59:08 +0900

    [level 1] Title: 카드 뭉치, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> cards1, vector<string> cards2, vector<string> goal)
+{
+    string answer = "Yes";
+    
+    int idx1 = 0;
+    int idx2 = 0;
+    
+    for (int i = 0; i < goal.size(); ++i)
+    {
+        if (idx1 < cards1.size())
+        {
+            if (cards1[idx1] == goal[i])
+            {
+                idx1 += 1;
+                continue;
+            }
+        }
+        if (idx2 < cards2.size())
+        {
+            if (cards2[idx2] == goal[i])
+            {
+                idx2 += 1;
+                continue;
+            }
+        }
+        
+        answer = "No";
+    }
+    
+    return answer;
+}
