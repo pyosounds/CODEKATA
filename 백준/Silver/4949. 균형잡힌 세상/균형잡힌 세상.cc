f28856ff853a8e9843dfc7caaa56deeaa75c855d
pyosounds <156515622+pyosounds@users.noreply.github.com>
2026-01-09 20:54:23 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	while (true)
+	{
+		string str;
+		getline(cin, str);
+
+		if (str == ".")
+		{
+			break;
+		}
+
+		stack<char> s;
+		bool check = true;
+
+		for (int i = 0; i < str.size(); ++i)
+		{
+			if (str[i] == ')')
+			{
+				if (!s.empty() && s.top() == '(')
+				{
+					s.pop();
+				}
+				else
+				{
+					check = false;
+					break;
+				}
+			}
+			else if (str[i] == ']')
+			{
+				if (!s.empty() && s.top() == '[')
+				{
+					s.pop();
+				}
+				else
+				{
+					check = false;
+					break;
+				}
+			}
+			else if (str[i] == '(' || str[i] == '[')
+			{
+				s.push(str[i]);
+			}
+		}
+
+		if (check && s.empty())
+		{
+			cout << "yes\n";
+		}
+		else
+		{
+			cout << "no\n";
+		}
+	}
+
+	return 0;
+}
